bede4e27ecaa196ff3e5e905a54284dd0a5dbeaf
Cassiopeia316 <dvinagunawan@gmail.com>
2022-05-31 00:12:30 +0700

    card

--- src/components/Carcard/index.jsx
+++ src/components/Carcard/index.jsx
@@ -11,11 +11,11 @@ const Carcard = ({ data }) => {
                 <img src={image} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h6 className="card-title">{name} / {category}</h6>
-                    <h5 className="card-text">Rp. {price.toLocaleString()}</h5>
+                    <h5 className="card-text">Rp. {price?.toLocaleString()}</h5>
                     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <p> <i className="fa-solid fa-user-group"></i> &nbsp;4 orang </p>
                     <p> <i className="fa-solid fa-gear"></i> &nbsp; Manual </p>
-                    <p> <i className="fa-solid fa-calendar"></i> &nbsp; Tahun {start_rent_at ? start_rent_at.slice(0,4) : null} </p>
+                    <p> <i className="fa-solid fa-calendar"></i> &nbsp; Tahun {start_rent_at?.slice(0,4)} </p>
                     <Link to={`/detail/${id}`} className="btn btn-primary">Pilih mobil</Link>
                 </div>                     
             </Content>
